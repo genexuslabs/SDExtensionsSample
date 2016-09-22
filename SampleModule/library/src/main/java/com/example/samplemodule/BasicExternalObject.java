132f1e2513e3f4fca376c0989f1f18c686491fee
juandiana <juandiana@gmail.com>
2016-09-21 22:00:20 -0300

    Use the more generic IMethodInvoker interface instead of ISimpleMethodInvoker

--- SampleModule/library/src/main/java/com/example/samplemodule/BasicExternalObject.java
+++ SampleModule/library/src/main/java/com/example/samplemodule/BasicExternalObject.java
@@ -2,32 +2,36 @@ package com.example.samplemodule;
 
 import java.util.List;
 
+import android.support.annotation.NonNull;
+
+import com.artech.actions.ActionResult;
 import com.artech.application.MyApplication;
 import com.artech.externalapi.ExternalApi;
+import com.artech.externalapi.ExternalApiResult;
 
 public class BasicExternalObject extends ExternalApi {
 	private static final String METHOD_HELLO = "Hello";
 	private static final String METHOD_MESSAGE = "Message";
 
 	public BasicExternalObject() {
-		addSimpleMethodHandler(METHOD_HELLO, 0, mMethodHello);
-		addSimpleMethodHandler(METHOD_MESSAGE, 1, mMethodMessage);
+		addMethodHandler(METHOD_HELLO, 0, mMethodHello);
+		addMethodHandler(METHOD_MESSAGE, 1, mMethodMessage);
 	}
 
-	private final ISimpleMethodInvoker mMethodHello = new ISimpleMethodInvoker() {
+	private final IMethodInvoker mMethodHello = new IMethodInvoker() {
 		@Override
-		public Object invoke(List<Object> parameters) {
+		public @NonNull ExternalApiResult invoke(List<Object> parameters) {
 			MyApplication.getInstance().showMessage(getContext().getString(com.example.genexusmodule.R.string.hello_message));
-			return null;
+			return new ExternalApiResult(ActionResult.SUCCESS_CONTINUE);
 		}
 	};
 
-	private final ISimpleMethodInvoker mMethodMessage = new ISimpleMethodInvoker() {
+	private final IMethodInvoker mMethodMessage = new IMethodInvoker() {
 		@Override
-		public Object invoke(List<Object> parameters) {
+		public @NonNull ExternalApiResult invoke(List<Object> parameters) {
 			final String text = (String) parameters.get(0);
 			MyApplication.getInstance().showMessage(text);
-			return null;
+			return new ExternalApiResult(ActionResult.SUCCESS_CONTINUE);
 		}
 	};
 }
