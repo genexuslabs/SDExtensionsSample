f2f9a163f838c7516a09145d12b44bf2a2d3df22
Cristian Inthamoussu <cinthamo@gmail.com>
2017-11-08 13:54:20 -0300

    Use AppCompatTextView for parent of BasicUserControl because it is needed when creating custom views

--- SampleModule/library/src/main/java/com/example/samplemodule/BasicUserControl.java
+++ SampleModule/library/src/main/java/com/example/samplemodule/BasicUserControl.java
@@ -5,8 +5,8 @@ import java.util.List;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
-import android.widget.TextView;
 
 import com.artech.base.controls.IGxControlRuntime;
 import com.artech.base.metadata.ActionDefinition;
@@ -17,7 +17,7 @@ import com.artech.ui.Coordinator;
 import com.example.genexusmodule.R;
 
 @SuppressLint("ViewConstructor")
-public class BasicUserControl extends TextView implements IGxEdit,
+public class BasicUserControl extends AppCompatTextView implements IGxEdit,
 														  IGxControlRuntime {
 	final static String NAME = "BasicUserControl";
 	private final static String METHOD_SET_NAME = "SetName";
