a7bbfce79158214303ec556e3ea85a857acfb2d2
Cinthya C Riveros <cinthya.riveros@globant.com>
2023-03-17 14:12:23 -0300

    Avoid message from Main Thread checker

--- SampleLibrary/iOS/BasicExternalObject.swift
+++ SampleLibrary/iOS/BasicExternalObject.swift
@@ -71,27 +71,27 @@ public class BasicExternalObjectOffline: GXExternalObjectBase {
 fileprivate class ToastMessage {
 	static func showToast(message : String) {
 		// Ideas from https://stackoverflow.com/questions/31540375/how-to-toast-message-in-swift
-		// First get the controller and it's view
-		if let rootController = GXExecutionEnvironmentHelper.keyWindow?.rootViewController {
-			// Build the Toast sample
-			let size = rootController.view.frame.size
-			let toastLabel = UILabel(frame: CGRect(x: (size.width)/2 - 75, y: (size.height) - 100, width: 150, height: 35))
-			toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-			toastLabel.textColor = UIColor.white
-			toastLabel.textAlignment = .center;
-			toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
-			toastLabel.text = message
-			toastLabel.alpha = 1.0
-			toastLabel.layer.cornerRadius = 10;
-			toastLabel.clipsToBounds  =  true
-			
-			rootController.view.addSubview(toastLabel)
-			
-			UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
-				toastLabel.alpha = 0.0
-			}, completion: {(isCompleted) in
-				toastLabel.removeFromSuperview()
-			})
+		DispatchQueue.main.async {
+			// First get the controller and it's view
+			if let rootController = GXExecutionEnvironmentHelper.keyWindow?.rootViewController {
+				// Build the Toast sample
+				let size = rootController.view.frame.size
+				let toastLabel = UILabel(frame: CGRect(x: (size.width)/2 - 75, y: (size.height) - 100, width: 150, height: 35))
+				toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+				toastLabel.textColor = UIColor.white
+				toastLabel.textAlignment = .center;
+				toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+				toastLabel.text = message
+				toastLabel.alpha = 1.0
+				toastLabel.layer.cornerRadius = 10;
+				toastLabel.clipsToBounds  =  true
+				rootController.view.addSubview(toastLabel)
+				UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+					toastLabel.alpha = 0.0
+				}, completion: {(isCompleted) in
+					toastLabel.removeFromSuperview()
+				})
+			}
 		}
 	}
 }
