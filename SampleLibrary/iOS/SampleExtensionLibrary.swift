dc959ccc2a438ecf318d63cc723ccc6dac8456b6
Cinthya C Riveros <cinthya.riveros@globant.com>
2023-03-17 11:29:51 -0300

    Update user control register fixed

--- SampleLibrary/iOS/SampleExtensionLibrary.swift
+++ SampleLibrary/iOS/SampleExtensionLibrary.swift
@@ -7,6 +7,7 @@ public class SampleExtensionLibrary: NSObject, GXExtensionLibraryProtocol {
 	public func initializeExtensionLibrary(withContext context: GXExtensionLibraryContext) {
 
 		GXActionExternalObjectHandler.register(BasicExternalObject.self, forExternalObjectName:BasicExternalObject.classIdentifier)
+		GXControlFactory.register(controlClass: BasicUserControl.self, forUserControlType: "BasicUserControl")
 
 	}
 }
