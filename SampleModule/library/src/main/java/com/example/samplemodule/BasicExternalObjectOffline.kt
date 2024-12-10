c180c3b5b3b53349064ffeabc509963b362b1c9f
alfoirazabal <alfoirazabal@live.com>
2024-12-10 12:08:36 -0300

    Add offline method implementations.
    `addNumbers` and `printMessage`

--- SampleModule/library/src/main/java/com/example/samplemodule/BasicExternalObjectOffline.kt
+++ SampleModule/library/src/main/java/com/example/samplemodule/BasicExternalObjectOffline.kt
@@ -1,5 +1,8 @@
 package com.example.samplemodule
 
+import com.example.samplemodule.BasicExternalObject.Companion.EVENT_NAME_ON_MESSAGE_PRINTED
+import com.example.samplemodule.BasicExternalObject.Companion.NAME
+import com.genexus.android.core.actions.ExternalObjectEvent
 import com.genexus.android.core.base.services.Services
 
 class BasicExternalObjectOffline {
@@ -11,7 +14,19 @@ class BasicExternalObjectOffline {
 
 		@JvmStatic
 		fun message(text: String) {
-			Services.Messages.showMessage(text)
+			Services.Messages.showMessage(text + "V1")
+		}
+
+		@JvmStatic
+		fun addNumbers(firstAddend: Int?, secondAddend: Int?) : Int {
+			return (firstAddend?:0) + (secondAddend?:0)
+		}
+
+		@JvmStatic
+		fun printMessage(message: String) {
+			val event = ExternalObjectEvent(NAME, EVENT_NAME_ON_MESSAGE_PRINTED)
+			val params = listOf(message)
+			event.fire(params)
 		}
 	}
 }
